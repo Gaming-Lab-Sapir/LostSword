fd6982f4c0941091458b7dbfb177650be4024d15
Eli_Alhazov <elialhazov20@gmail.com>
2025-09-28 21:48:09 +0300

    add potions with scriptable objects

--- Assets/Scripts/Game/Items/PickupItem.cs
+++ Assets/Scripts/Game/Items/PickupItem.cs
@@ -4,7 +4,7 @@
 public class PickupItem : MonoBehaviour
 {
     public ItemSO item;
-    public int amount = 1;        
+    public int amount = 1;
     public bool destroyOnPickup = true;
 
     private void Reset()
@@ -20,16 +20,37 @@ public class PickupItem : MonoBehaviour
         var invHolder = other.GetComponent<PlayerInventory>();
         if (invHolder == null || item == null) return;
 
-        invHolder.inventory.AddItem(item, amount);
-
         switch (item.type)
         {
             case ItemType.Coin:
+                invHolder.inventory.AddItem(item, amount);
                 GameEvents.RaiseCoinCollected(amount);
                 break;
 
             case ItemType.Ammo:
+                invHolder.inventory.AddItem(item, item.ammoAmount);
                 GameEvents.RaiseArrowPickedUp(item.ammoAmount);
+                break;
+
+            case ItemType.HealthPotion:
+                if (item.healAmount > 0)
+                {
+                    GameEvents.RaisePlayerDamaged(-item.healAmount);
+                }
+                else if (item.healAmount < 0)
+                {
+                    var hp = other.GetComponent<PlayerHealth>();
+                    if (hp != null)
+                        hp.ForceDamage(-item.healAmount);
+                    else
+                        GameEvents.RaisePlayerDamaged(-item.healAmount);
+                }
+                break;
+
+            case ItemType.QuestItem:
+            case ItemType.Other:
+                invHolder.inventory.AddItem(item, amount);
+                Debug.Log($"{item.displayName} collected");
                 break;
         }
 
