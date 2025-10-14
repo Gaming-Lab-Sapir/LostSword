79b13f8b797aca16bef43cfbec19c16b6238218b
Eli_Alhazov <elialhazov20@gmail.com>
2025-10-14 21:31:06 +0300

    add level3HUDController that display gates,swords and complete level

---
+++ Assets/Scripts/UI/Level3HUDController.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+public class Level3HUDController : MonoBehaviour
+{
+    [SerializeField] TMP_Text gatesText;
+    [SerializeField] TMP_Text swordText;
+    [SerializeField] TMP_Text levelCompleteText;
+    [SerializeField] string questIdFilter = "Level3"; 
+
+    void OnEnable()
+    {
+        GameEvents.GatesProgressChanged += OnGates;
+        GameEvents.SwordProgressChanged += OnSword;
+        GameEvents.QuestCompleted += OnQuestCompleted;
+
+        if (levelCompleteText) levelCompleteText.gameObject.SetActive(false);
+        if (gatesText) gatesText.text = "Gates: 0/2";
+        if (swordText) swordText.text = "Sword: 0/1";
+    }
+
+    void OnDisable()
+    {
+        GameEvents.GatesProgressChanged -= OnGates;
+        GameEvents.SwordProgressChanged -= OnSword;
+        GameEvents.QuestCompleted -= OnQuestCompleted;
+    }
+
+    void OnGates(int current, int total) => gatesText.text = $"Gates: {current}/{total}";
+    void OnSword(int current, int total) => swordText.text = $"Sword: {current}/{total}";
+
+    void OnQuestCompleted(string questId)
+    {
+        if (!questId.ToLower().Contains(questIdFilter.ToLower())) return;
+        if (levelCompleteText)
+        {
+            levelCompleteText.text = "LEVEL 3 COMPLETE";
+            levelCompleteText.gameObject.SetActive(true);
+        }
+    }
+}
